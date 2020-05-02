fd4b3d6d6816ce2e229fa6802e0cfe7220e78b88
Paul Henschel <drcmda@gmail.com>
2020-05-02 12:40:07 +0200

    updates deps

--- example/src/Model.js
+++ example/src/Model.js
@@ -6,22 +6,13 @@ source: https://sketchfab.com/models/2ef4c45caa35450db1b876a7f94ff79d
 title: Thor and the Midgard Serpent
 */
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useLoader } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { draco } from 'drei'
 
 export default function Model(props) {
-  const { nodes, materials } = useLoader(GLTFLoader, '/scene_draco.glb', loader => {
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('/draco-gltf/')
-    loader.setDRACOLoader(dracoLoader)
-  })
-
-  const [hovered, setHovered] = useState()
-  const [active, setActive] = useState()
-  useEffect(() => void (document.body.style.cursor = hovered ? 'pointer' : 'auto'), [hovered])
-
+  const { nodes, materials } = useLoader(GLTFLoader, '/scene_draco.glb', draco())
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={[0.01, 0.01, 0.01]}>
@@ -33,11 +24,7 @@ export default function Model(props) {
             <mesh
               material={materials.SerpentBake}
               geometry={nodes.Circle011_SerpentBake_0.geometry}
-              onClick={() => setActive(!active)}
-              onPointerOver={e => (e.stopPropagation(), setHovered(true))}
-              onPointerOut={() => setHovered(false)}
-              material-metalness={active ? 1 : 0}
-              material-color={hovered ? 'lightblue' : 'white'}
+              material-metalness={0}
             />
           </group>
           <group position={[-1018.2, -380.5, 1332.6]} rotation={[-1, 0.72, -0.08]} scale={[100, 100, 100]}>
@@ -52,7 +39,7 @@ export default function Model(props) {
             </mesh>
             <mesh material={materials.Boat1Bake} geometry={nodes.Keel002_Boat1Bake_0.geometry} />
           </group>
-          <group position={[0, 27, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={[100, 100, 170]}></group>
+          <group position={[0, 27, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={[100, 100, 170]} />
           <group rotation={[-Math.PI / 2, 0, 0]} scale={[100, 100, 100]}>
             <mesh material={materials.RockBake} geometry={nodes.Rock021_RockBake_0.geometry} material-metalness={0.5} />
           </group>
