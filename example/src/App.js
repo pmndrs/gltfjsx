6d24afc4b81e92bf18c6448f9f3868a60f25e082
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-09-26 17:39:01 +0200

    Updates drei to new react-three namespace, modernizes example

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,15 @@
 import * as THREE from 'three'
 import React, { Suspense, useRef } from 'react'
-import { Canvas, Dom, extend, useLoader, useThree, useFrame } from 'react-three-fiber'
-import lerp from 'lerp'
+import { Canvas, useFrame } from 'react-three-fiber'
+import { Html, Loader } from '@react-three/drei'
+
 import Model from './Model'
 
 function Rig({ children }) {
   const outer = useRef()
   const inner = useRef()
   useFrame(({ clock }) => {
-    outer.current.position.y = lerp(outer.current.position.y, 0, 0.05)
+    outer.current.position.y = THREE.MathUtils.lerp(outer.current.position.y, 0, 0.05)
     inner.current.rotation.y = Math.sin(clock.getElapsedTime() / 8) * Math.PI
     inner.current.position.z = 5 + -Math.sin(clock.getElapsedTime() / 2) * 10
     inner.current.position.y = -5 + Math.sin(clock.getElapsedTime() / 2) * 2
@@ -28,15 +29,21 @@ export default function App() {
       camera={{ position: [0, 15, 30], fov: 70 }}
       onCreated={({ gl, camera }) => {
         camera.lookAt(0, 0, 0)
-        gl.setClearColor('#fff0ea')
-        gl.toneMapping = THREE.Uncharted2ToneMapping
       }}>
+      <color attach="background" args={['#fff0ea']} />
       <fog attach="fog" args={['#fff0ea', 10, 60]} />
       <ambientLight intensity={6} />
-      <Suspense fallback={<Dom center>loading ...</Dom>}>
+
+      <Suspense
+        fallback={
+          <Html center>
+            <Loader />
+          </Html>
+        }>
         <Rig>
           <Model />
-          <mesh scale={[1000, 1000, 1]} rotation={[-Math.PI / 2, 0, 0]} onPointerOver={e => e.stopPropagation()}>
+
+          <mesh scale={[1000, 1000, 1]} rotation={[-Math.PI / 2, 0, 0]} onPointerOver={(e) => e.stopPropagation()}>
             <planeBufferGeometry attach="geometry" />
             <meshBasicMaterial attach="material" transparent opacity={0.7} color="skyblue" />
           </mesh>
