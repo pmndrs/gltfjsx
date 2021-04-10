665073332b0cebecebb67bc3965a4eb54e5b8cb9
Paul Henschel <drcmda@gmail.com>
2021-04-10 12:31:13 +0200

    fix precision

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -2,11 +2,27 @@ const THREE = require('three')
 const prettier = require('prettier')
 const parserBabel = require('prettier/parser-babel')
 const isVarName = require('./isVarName')
-const { rNbr, rDeg } = require('./rotation')
 
 function parse(fileName, gltf, options = {}) {
   function sanitizeName(name) {
     return isVarName(name) ? `.${name}` : `['${name}']`
+  }
+
+  const rNbr = (number) => {
+    return parseFloat(number.toFixed(Math.round(options.precision || 2)))
+  }
+
+  const rDeg = (number, precision) => {
+    const abs = Math.abs(Math.round(parseFloat(number) * 100000))
+    for (let i = 1; i <= 10; i++) {
+      if (abs === Math.round(parseFloat(Math.PI / i) * 100000))
+        return `${number < 0 ? '-' : ''}Math.PI${i > 1 ? ' / ' + i : ''}`
+    }
+    for (let i = 1; i <= 10; i++) {
+      if (abs === Math.round(parseFloat(Math.PI * i) * 100000))
+        return `${number < 0 ? '-' : ''}Math.PI${i > 1 ? ' * ' + i : ''}`
+    }
+    return rNbr(number)
   }
 
   function printTypes(objects, animations) {
