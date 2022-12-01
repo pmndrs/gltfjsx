e3d1be0df6086d03511e8c917e8c9f1fd1b99b20
Max Rusan <max@maxtherocket.com>
2022-11-30 23:59:28 -0500

    Added GPU instancing support (by default)

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -1,15 +1,17 @@
 import { cpus } from 'os'
 import { NodeIO } from '@gltf-transform/core'
-import { dedup, resample, prune, textureResize, webp } from '@gltf-transform/functions'
+import { dedup, resample, prune, textureResize, webp, instance } from '@gltf-transform/functions'
 import squoosh from '@squoosh/lib'
-import { DracoMeshCompression, KHRONOS_EXTENSIONS } from '@gltf-transform/extensions'
+import { DracoMeshCompression, KHRONOS_EXTENSIONS, MeshGPUInstancing } from '@gltf-transform/extensions'
 import draco3d from 'draco3dgltf'
 
 async function transform(file, output, config = {}) {
-  const io = new NodeIO().registerExtensions([DracoMeshCompression, ...KHRONOS_EXTENSIONS]).registerDependencies({
-    'draco3d.decoder': await draco3d.createDecoderModule(),
-    'draco3d.encoder': await draco3d.createEncoderModule(),
-  })
+  const io = new NodeIO()
+    .registerExtensions([DracoMeshCompression, ...KHRONOS_EXTENSIONS, MeshGPUInstancing])
+    .registerDependencies({
+      'draco3d.decoder': await draco3d.createDecoderModule(),
+      'draco3d.encoder': await draco3d.createEncoderModule(),
+    })
 
   const document = await io.read(file)
 
@@ -20,6 +22,7 @@ async function transform(file, output, config = {}) {
     resample(),
     // Remove unused nodes, textures, or other data.
     prune(),
+    instance(),
     // Resize all textures to ≤1K.
     textureResize({ size: [1024, 1024] }),
     // Convert textures to WebP
