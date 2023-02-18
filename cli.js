pmndrs/gltfjsx

commit 94ad0321364561bf07ca47c5ab808871eabc08bb

V6 (#172)
* Converted the cli tool to ESM and added WebP compression support

* yarn.lock

* Fixed relatve file path isssue

* Upgraded version of meow

* 1.0.2

* 1.0.3

* 1.0.4

* 1.0.5

* 1.0.6

* Added GPU instancing support (by default)

* 1.0.7

* Update draco3dgltf (#169)

* 5.0.3

* Add option for output file path/name (#163)

* 5.0.4

* fix: missing useGLTF when instancesall === true

* cleanup

* 6.0.0

Co-authored-by: Max Rusan <max@maxtherocket.com>
Co-authored-by: Yiorgis Gozadinos <ggozadinos@gmail.com>
Co-authored-by: Matt Black <mattblackdev@users.noreply.github.com>

cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 'use strict'
-const React = require('react')
-const importJsx = require('import-jsx')
-const { render } = require('ink')
-const meow = require('meow')
+import meow from 'meow'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import gltfjsx from './src/gltfjsx.js'
 
-const App = importJsx('./src/components/App')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const cli = meow(
   `
@@ -26,10 +28,10 @@ const cli = meow(
     --instance, -i      Instance re-occuring geometry
     --instanceall, -I   Instance every geometry (for cheaper re-use)
     --transform, -T     Transform the asset for the web (draco, prune, resize)
-    --aggressive, -a    Aggressively prune the graph (empty groups, transform overlap) 
     --debug, -D         Debug output
 `,
   {
+    importMeta: import.meta,
     flags: {
       output: { type: 'string', alias: 'o' },
       types: { type: 'boolean', alias: 't' },
@@ -44,7 +46,6 @@ const cli = meow(
       instance: { type: 'boolean', alias: 'i' },
       instanceall: { type: 'boolean', alias: 'I' },
       transform: { type: 'boolean', alias: 'T' },
-      aggressive: { type: 'boolean', alias: 'a' },
       debug: { type: 'boolean', alias: 'D' },
     },
   }
@@ -53,5 +54,18 @@ const cli = meow(
 if (cli.input.length === 0) {
   console.log(cli.help)
 } else {
-  render(React.createElement(App, { file: cli.input[0], ...cli.flags }))
+  const config = { ...cli.flags }
+  const file = cli.input[0]
+  const filePath = path.resolve(__dirname, file)
+  let nameExt = file.match(/[-_\w]+[.][\w]+$/i)[0]
+  let name = nameExt.split('.').slice(0, -1).join('.')
+  const output = name.charAt(0).toUpperCase() + name.slice(1) + (config.types ? '.tsx' : '.js')
+  const showLog = (log) => {
+    console.info('log:', log)
+  }
+  try {
+    const response = await gltfjsx(file, output, { ...config, showLog, timeout: 0, delay: 1 })
+  } catch (e) {
+    console.error(e)
+  }
 }

example/.gitignore
@@ -1,23 +1,0 @@
-# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
-
-# dependencies
-/node_modules
-/.pnp
-.pnp.js
-
-# testing
-/coverage
-
-# production
-/build
-
-# misc
-.DS_Store
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*

example/config-overrides.js
@@ -1,4 +1,0 @@
-const { override } = require('customize-cra')
-const { addReactRefresh } = require('customize-cra-react-refresh')
-
-module.exports = override(addReactRefresh())

example/public/index.html
@@ -1,43 +1,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="Web site created using create-react-app"
-    />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-    <!--
-      manifest.json provides metadata used when your web app is installed on a
-      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-    -->
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <!--
-      Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML.
-
-      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-      work correctly both with client-side routing and a non-root public URL.
-      Learn how to configure a non-root public URL by running `npm run build`.
-    -->
-    <title>React App</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-    <!--
-      This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
-
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the <body> tag.
-
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build`.
-    -->
-  </body>
-</html>

commit a3e745f8cc1946600713a9e7f01dfddc89ef235d

Fix `output` command-line flag (#188)
* Fix output command-line option that was broken by 94ad0321364561bf07ca47c5ab808871eabc08bb

* Remove unused filePath

cli.js
@@ -73,10 +73,9 @@ if (cli.input.length === 0) {
 Command: npx gltfjsx@${packageJson.version} ${process.argv.slice(2).join(' ')}`,
   }
   const file = cli.input[0]
-  const filePath = path.resolve(__dirname, file)
   let nameExt = file.match(/[-_\w]+[.][\w]+$/i)[0]
   let name = nameExt.split('.').slice(0, -1).join('.')
-  const output = name.charAt(0).toUpperCase() + name.slice(1) + (config.types ? '.tsx' : '.jsx')
+  const output = config.output ?? name.charAt(0).toUpperCase() + name.slice(1) + (config.types ? '.tsx' : '.jsx')
   const showLog = (log) => {
     console.info('log:', log)
   }