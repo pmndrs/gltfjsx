c8b5851d67cbf43954156db61e724fc7b1bce558
— <drcmda@gmail.com>
2023-06-23 16:35:48 +0200

    fix: specular glossiness crash

--- src/bin/GLTFLoader.js
+++ src/bin/GLTFLoader.js
@@ -665,154 +665,6 @@ GLTFTextureTransformExtension.prototype.extendTexture = function (texture, trans
  * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_pbrSpecularGlossiness
  */
 
-/**
- * A sub class of THREE.StandardMaterial with some of the functionality
- * changed via the `onBeforeCompile` callback
- * @pailhead
- */
-
-function GLTFMeshStandardSGMaterial(params) {
-  THREE.MeshStandardMaterial.call(this)
-
-  this.isGLTFSpecularGlossinessMaterial = true
-
-  //various chunks that need replacing
-  var specularMapParsFragmentChunk = ['#ifdef USE_SPECULARMAP', '	uniform sampler2D specularMap;', '#endif'].join('\n')
-
-  var glossinessMapParsFragmentChunk = ['#ifdef USE_GLOSSINESSMAP', '	uniform sampler2D glossinessMap;', '#endif'].join(
-    '\n'
-  )
-
-  var specularMapFragmentChunk = [
-    'vec3 specularFactor = specular;',
-    '#ifdef USE_SPECULARMAP',
-    '	vec4 texelSpecular = texture2D( specularMap, vUv );',
-    '	texelSpecular = sRGBToLinear( texelSpecular );',
-    '	// reads channel RGB, compatible with a glTF Specular-Glossiness (RGBA) texture',
-    '	specularFactor *= texelSpecular.rgb;',
-    '#endif',
-  ].join('\n')
-
-  var glossinessMapFragmentChunk = [
-    'float glossinessFactor = glossiness;',
-    '#ifdef USE_GLOSSINESSMAP',
-    '	vec4 texelGlossiness = texture2D( glossinessMap, vUv );',
-    '	// reads channel A, compatible with a glTF Specular-Glossiness (RGBA) texture',
-    '	glossinessFactor *= texelGlossiness.a;',
-    '#endif',
-  ].join('\n')
-
-  var lightPhysicalFragmentChunk = [
-    'PhysicalMaterial material;',
-    'material.diffuseColor = diffuseColor.rgb * ( 1. - max( specularFactor.r, max( specularFactor.g, specularFactor.b ) ) );',
-    'vec3 dxy = max( abs( dFdx( geometryNormal ) ), abs( dFdy( geometryNormal ) ) );',
-    'float geometryRoughness = max( max( dxy.x, dxy.y ), dxy.z );',
-    'material.specularRoughness = max( 1.0 - glossinessFactor, 0.0525 ); // 0.0525 corresponds to the base mip of a 256 cubemap.',
-    'material.specularRoughness += geometryRoughness;',
-    'material.specularRoughness = min( material.specularRoughness, 1.0 );',
-    'material.specularColor = specularFactor;',
-  ].join('\n')
-
-  var uniforms = {
-    specular: { value: new THREE.Color().setHex(0xffffff) },
-    glossiness: { value: 1 },
-    specularMap: { value: null },
-    glossinessMap: { value: null },
-  }
-
-  this._extraUniforms = uniforms
-
-  this.onBeforeCompile = function (shader) {
-    for (var uniformName in uniforms) {
-      shader.uniforms[uniformName] = uniforms[uniformName]
-    }
-
-    shader.fragmentShader = shader.fragmentShader
-      .replace('uniform float roughness;', 'uniform vec3 specular;')
-      .replace('uniform float metalness;', 'uniform float glossiness;')
-      .replace('#include <roughnessmap_pars_fragment>', specularMapParsFragmentChunk)
-      .replace('#include <metalnessmap_pars_fragment>', glossinessMapParsFragmentChunk)
-      .replace('#include <roughnessmap_fragment>', specularMapFragmentChunk)
-      .replace('#include <metalnessmap_fragment>', glossinessMapFragmentChunk)
-      .replace('#include <lights_physical_fragment>', lightPhysicalFragmentChunk)
-  }
-
-  Object.defineProperties(this, {
-    specular: {
-      get: function () {
-        return uniforms.specular.value
-      },
-      set: function (v) {
-        uniforms.specular.value = v
-      },
-    },
-
-    specularMap: {
-      get: function () {
-        return uniforms.specularMap.value
-      },
-      set: function (v) {
-        uniforms.specularMap.value = v
-
-        if (v) {
-          this.defines.USE_SPECULARMAP = '' // USE_UV is set by the renderer for specular maps
-        } else {
-          delete this.defines.USE_SPECULARMAP
-        }
-      },
-    },
-
-    glossiness: {
-      get: function () {
-        return uniforms.glossiness.value
-      },
-      set: function (v) {
-        uniforms.glossiness.value = v
-      },
-    },
-
-    glossinessMap: {
-      get: function () {
-        return uniforms.glossinessMap.value
-      },
-      set: function (v) {
-        uniforms.glossinessMap.value = v
-
-        if (v) {
-          this.defines.USE_GLOSSINESSMAP = ''
-          this.defines.USE_UV = ''
-        } else {
-          delete this.defines.USE_GLOSSINESSMAP
-          delete this.defines.USE_UV
-        }
-      },
-    },
-  })
-
-  delete this.metalness
-  delete this.roughness
-  delete this.metalnessMap
-  delete this.roughnessMap
-
-  this.setValues(params)
-}
-
-GLTFMeshStandardSGMaterial.prototype = Object.create(THREE.MeshStandardMaterial.prototype)
-GLTFMeshStandardSGMaterial.prototype.constructor = GLTFMeshStandardSGMaterial
-
-GLTFMeshStandardSGMaterial.prototype.copy = function (source) {
-  THREE.MeshStandardMaterial.prototype.copy.call(this, source)
-  this.specularMap = source.specularMap
-  this.specular.copy(source.specular)
-  this.glossinessMap = source.glossinessMap
-  this.glossiness = source.glossiness
-  delete this.metalness
-  delete this.roughness
-  delete this.metalnessMap
-  delete this.roughnessMap
-  return this
-}
-
 function GLTFMaterialsPbrSpecularGlossinessExtension() {
   return {
     name: EXTENSIONS.KHR_MATERIALS_PBR_SPECULAR_GLOSSINESS,
@@ -845,7 +697,7 @@ function GLTFMaterialsPbrSpecularGlossinessExtension() {
     ],
 
     getMaterialType: function () {
-      return GLTFMeshStandardSGMaterial
+      return THREE.MeshStandardMaterial
     },
 
     extendParams: function (materialParams, materialDef, parser) {
@@ -876,7 +728,7 @@ function GLTFMaterialsPbrSpecularGlossinessExtension() {
     },
 
     createMaterial: function (materialParams) {
-      var material = new GLTFMeshStandardSGMaterial(materialParams)
+      var material = new THREE.MeshStandardMaterial(materialParams)
       material.fog = true
 
       material.color = materialParams.color
@@ -1930,11 +1782,7 @@ GLTFParser.prototype.loadMaterial = function (materialIndex) {
   return Promise.all(pending).then(function () {
     var material
 
-    if (materialType === GLTFMeshStandardSGMaterial) {
-      material = extensions[EXTENSIONS.KHR_MATERIALS_PBR_SPECULAR_GLOSSINESS].createMaterial(materialParams)
-    } else {
-      material = new materialType(materialParams)
-    }
+    material = new materialType(materialParams)    
 
     if (materialDef.name) material.name = materialDef.name
 
