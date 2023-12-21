85079c71342cc2e21b4408b027956fe089508895
willkrakow <willkrakow@gmail.com>
2023-12-21 15:50:12 -0500

    Keep full filenames when filename includes whitespaces or digits (#209)
    * Keep full filenames when filename includes whitespaces or digits
    
    * Revert yarn.lock

--- cli.js
+++ cli.js
@@ -81,7 +81,7 @@ if (cli.input.length === 0) {
 Command: npx gltfjsx@${packageJson.version} ${process.argv.slice(2).join(' ')}`,
   }
   const file = cli.input[0]
-  let nameExt = file.match(/[-_\w]+[.][\w]+$/i)[0]
+  let nameExt = file.match(/[-_\w\d\s]+[.][\w]+$/i)[0]
   let name = nameExt.split('.').slice(0, -1).join('.')
   const output = config.output ?? name.charAt(0).toUpperCase() + name.slice(1) + (config.types ? '.tsx' : '.jsx')
   const showLog = (log) => {
